5dcf2545c6028f3f358a35501f2251e99ca97d2f
claudia <ceruleancee@gmail.com>
2023-03-08 19:25:07 -0600

    Add ifStatements.js

---
+++ ifStatements.js
@@ -0,0 +1,18 @@
+const x = 20;
+const y = 10;
+
+console.log('ifStatements.js')
+
+// == matches string
+if(x == 10){
+console.log('x == 10');
+}
+
+// === matches datatypes
+if(x === 10){
+console.log('x === 10');
+} else if(x > 10){
+    console.log('x > 10');
+}else{
+    console.log('x < 10');
+}
