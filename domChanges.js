654b4b9d745d3522600ec2958ff9da1da336c453
claudia <ceruleancee@gmail.com>
2023-03-14 01:00:38 -0500

    update domChanges.js

--- domChanges.js
+++ domChanges.js
@@ -11,13 +11,13 @@ ul.lastElementChild.innerHTML = '<h1>whats up!</h1>';
 const btn = document.querySelector('.btn');
 btn.style.background = 'blue';
 
-// clikck, mouseover, mouseout, etc. (look up mdn docs)
-btn.addEventListener('click', (e) => {
+// click, mouseover, mouseout, etc. (look up mdn docs)
+btn.addEventListener('mousedown', (e) => {
     e.preventDefault();
     console.log('click');
 //    console.log(e.target.className);
     document.querySelector('#my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
+//    document.querySelector('body').classList.add('bg-dark');
 //    document.querySelector('.items').lastElementChild.innerHTML = '<h1>hello</h1>';
-    ul.lastElementChild.innerHTML = '<h1>hello</h1>';
+    ul.lastElementChild.innerHTML = '<h1>whats up</h1>';
 })
